3a8038553fe779e202e19ebcbd23246df6657fdb
Saugat Adhikari <saugatadhikari87@gmail.com>
2023-07-11 21:35:01 +0545

    merge fixed

--- form-handler.php
+++ form-handler.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $name=$_POST['name'];
 $visitor_email=$_POST['email'];
@@ -17,24 +16,4 @@ $headers .="reply-To: $visitor_email \r\n";
 
 mail($to,$email_body,$headers)
     header("Location: contact.html");
-=======
-<?php
-$name=$_POST['name'];
-$visitor_email=$_POST['email'];
-
-$message=$_POST['message'];
-
-$email_from = 'info@yourwebsite.com';
-
-$email_body = "User Name: $name.\n".
-     "User Email: $visitor_email.\n".
-     "User Message: $message.\n";
-    
-    $to = "learningdigitally0@gmail.com";
-$headers = "From: $email_from \r\n";
-$headers .="reply-To: $visitor_email \r\n";
-
-mail($to,$email_body,$headers)
-    header("Location: contact.html");
->>>>>>> 244543e4b83f5cdb3dad1ae70573217680bd8638
 ?>
