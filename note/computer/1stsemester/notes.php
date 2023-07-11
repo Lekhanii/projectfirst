667e6d32661fc529e270e9ba07f3df702ef37579
arjunadhikary <reasonworld12345@gmail.com>
2023-07-11 22:59:55 +0545

    note/computer/1stsemester/notes.php

--- note/computer/1stsemester/notes.php
+++ note/computer/1stsemester/notes.php
@@ -1,5 +1,5 @@
 <?php
-$folderPath = 'notes/computer/1stsemester';
+$folderPath = '../../../notes/computer/1stsemester';
 $filenames = scandir($folderPath);
 
 ?>
@@ -8,9 +8,26 @@ $filenames = scandir($folderPath);
 <html>
 <head>
     <title>Notes </title>
+
 </head>
+<style>
+    .card {
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 10px;
+        margin-bottom: 10px;
+        background-color: #f9f9f9;
+    }
+
+    .card a {
+        text-decoration: none;
+        color: #333;
+    }
+</style>
 <body>
-    <h1>File List</h1>
+    <h1>1st Semester</h1>
+    
+<div class="card">
     <?php foreach ($filenames as $filename): ?>
         <?php if ($filename !== '.' && $filename !== '..'): ?>
             <div>
@@ -18,5 +35,7 @@ $filenames = scandir($folderPath);
             </div>
         <?php endif; ?>
     <?php endforeach; ?>
+</div>
+
 </body>
 </html>
