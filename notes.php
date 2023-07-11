f9c4e811b2d60e99ddd9f9fa5bcafd954ba0efc8
Lekhani <anjalishah063@gmail.com>
2023-07-11 22:27:16 +0545

    notes.php added

---
+++ notes.php
@@ -0,0 +1,22 @@
+<?php
+$folderPath = '/notes/computer/1stsemester';
+$filenames = scandir($folderPath);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Notes </title>
+</head>
+<body>
+    <h1>File List</h1>
+    <?php foreach ($filenames as $filename): ?>
+        <?php if ($filename !== '.' && $filename !== '..'): ?>
+            <div>
+                <a href="<?php echo $folderPath . '/' . $filename; ?>" download><?php echo $filename; ?></a>
+            </div>
+        <?php endif; ?>
+    <?php endforeach; ?>
+</body>
+</html>
