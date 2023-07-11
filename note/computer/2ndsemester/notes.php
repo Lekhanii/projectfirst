c49da84313d2f3e1b166fcbaffa8f385caaec7d2
arjunadhikary <reasonworld12345@gmail.com>
2023-07-11 23:05:37 +0545

    feat: :bug: added second semester files

---
+++ note/computer/2ndsemester/notes.php
@@ -0,0 +1,41 @@
+<?php
+$folderPath = '../../../notes/computer/2ndsemester';
+$filenames = scandir($folderPath);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Notes </title>
+
+</head>
+<style>
+    .card {
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 10px;
+        margin-bottom: 10px;
+        background-color: #f9f9f9;
+    }
+
+    .card a {
+        text-decoration: none;
+        color: #333;
+    }
+</style>
+<body>
+    <h1>2nd Semester</h1>
+    
+<div class="card">
+    <?php foreach ($filenames as $filename): ?>
+        <?php if ($filename !== '.' && $filename !== '..'): ?>
+            <div>
+                <a href="<?php echo $folderPath . '/' . $filename; ?>" download><?php echo $filename; ?></a>
+            </div>
+        <?php endif; ?>
+    <?php endforeach; ?>
+</div>
+
+</body>
+</html>
